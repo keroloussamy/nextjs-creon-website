2d1503bb0a572385b3874a59d894aa01f8b6658c
keroloussamy <keroloussamy98@gmail.com>
2024-05-07 00:57:50 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
-import Image from "next/image";
+import ComingSoon from "@/components/ComingSoon";
+import CreonPass from "@/components/CreonPass";
+import Footer from "@/components/Footer";
+import Frame from "@/components/Frame";
+import Hero from "@/components/Hero";
+import Navbar from "@/components/Navbar";
+import OurMission from "@/components/OurMission";
+import Profiting from "@/components/Profiting";
 
 export default function Home() {
   return (
-    <main>Hello</main>
+    <>
+      <Navbar />
+      <Hero />
+      <CreonPass />
+      <Profiting />
+      <OurMission />
+      <ComingSoon />
+      <Frame />
+      <Footer />
+    </>
   );
 }
